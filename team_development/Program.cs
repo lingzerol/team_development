fa18c7347901026f53e5f28e22de86fa174e381c
Killbugwuhaunwei <Killbugwuhaunwei@outlook.com>
2019-04-21 10:13:11 +0800

    h

--- team_development/Program.cs
+++ team_development/Program.cs
@@ -18,7 +18,7 @@ namespace team_development
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1());         
         }
     }
 }
