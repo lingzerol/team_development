2da478b9ce225082d5f341977d668dfd17bf29f8
Killbugwuhaunwei <Killbugwuhaunwei@outlook.com>
2019-04-21 11:44:02 +0800

    4.21 2 whw

---
+++ team_development/InquiryNotification.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace team_development
+{
+    partial class InquiryNotification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+        
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.通知 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.发布单位 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("宋体", 31.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "通知信息";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.通知,
+            this.发布单位,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(37, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(949, 340);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // 通知
+            // 
+            this.通知.DataPropertyName = "通知";
+            this.通知.HeaderText = "通知";
+            this.通知.Name = "通知";
+            this.通知.Width = 400;
+            // 
+            // 发布单位
+            // 
+            this.发布单位.HeaderText = "发布单位";
+            this.发布单位.Name = "发布单位";
+            this.发布单位.Width = 300;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "发布日期";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 200;
+            // 
+            // InquiryNotification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1096, 532);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "InquiryNotification";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 通知;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 发布单位;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
--- team_development/Program.cs
+++ team_development/Program.cs
@@ -20,7 +20,7 @@ namespace team_development
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new InquiryNotification());
            
         }
         
