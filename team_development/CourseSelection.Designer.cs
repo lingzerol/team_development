053531557bcf29ff91f0253438af5d68de8b7928
lingzerol <467264565@qq.com>
2019-04-21 16:18:22 +0800

    add course selection design

---
+++ team_development/CourseSelection.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace team_development
+{
+    partial class CourseSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.教务系统ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.选课ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查询培养方案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.写请假条ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.进入学校论坛ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查询成绩ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查询通知ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查询就业ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查询饭卡ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查询ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.用户信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CourseInfo = new System.Windows.Forms.DataGridView();
+            this.TimingSelection = new System.Windows.Forms.Button();
+            this.Selection = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CourseInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.教务系统ToolStripMenuItem,
+            this.写请假条ToolStripMenuItem,
+            this.进入学校论坛ToolStripMenuItem,
+            this.查询ToolStripMenuItem,
+            this.用户信息ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1096, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.MenuStrip_ItemClicked);
+            // 
+            // 教务系统ToolStripMenuItem
+            // 
+            this.教务系统ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.选课ToolStripMenuItem,
+            this.查询培养方案ToolStripMenuItem});
+            this.教务系统ToolStripMenuItem.Name = "教务系统ToolStripMenuItem";
+            this.教务系统ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.教务系统ToolStripMenuItem.Text = "教务系统";
+            // 
+            // 选课ToolStripMenuItem
+            // 
+            this.选课ToolStripMenuItem.Name = "选课ToolStripMenuItem";
+            this.选课ToolStripMenuItem.Size = new System.Drawing.Size(174, 26);
+            this.选课ToolStripMenuItem.Text = "选课";
+            // 
+            // 查询培养方案ToolStripMenuItem
+            // 
+            this.查询培养方案ToolStripMenuItem.Name = "查询培养方案ToolStripMenuItem";
+            this.查询培养方案ToolStripMenuItem.Size = new System.Drawing.Size(174, 26);
+            this.查询培养方案ToolStripMenuItem.Text = "查询培养方案";
+            // 
+            // 写请假条ToolStripMenuItem
+            // 
+            this.写请假条ToolStripMenuItem.Name = "写请假条ToolStripMenuItem";
+            this.写请假条ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.写请假条ToolStripMenuItem.Text = "写请假条";
+            this.写请假条ToolStripMenuItem.Click += new System.EventHandler(this.AFLToolStripMenuItem_Click);
+            // 
+            // 进入学校论坛ToolStripMenuItem
+            // 
+            this.进入学校论坛ToolStripMenuItem.Name = "进入学校论坛ToolStripMenuItem";
+            this.进入学校论坛ToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.进入学校论坛ToolStripMenuItem.Text = "进入学校论坛";
+            // 
+            // 查询ToolStripMenuItem
+            // 
+            this.查询ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.查询成绩ToolStripMenuItem,
+            this.查询通知ToolStripMenuItem,
+            this.查询就业ToolStripMenuItem,
+            this.查询饭卡ToolStripMenuItem,
+            this.查询ToolStripMenuItem1});
+            this.查询ToolStripMenuItem.Name = "查询ToolStripMenuItem";
+            this.查询ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.查询ToolStripMenuItem.Text = "查询";
+            // 
+            // 查询成绩ToolStripMenuItem
+            // 
+            this.查询成绩ToolStripMenuItem.Name = "查询成绩ToolStripMenuItem";
+            this.查询成绩ToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.查询成绩ToolStripMenuItem.Text = "查询成绩";
+            // 
+            // 查询通知ToolStripMenuItem
+            // 
+            this.查询通知ToolStripMenuItem.Name = "查询通知ToolStripMenuItem";
+            this.查询通知ToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.查询通知ToolStripMenuItem.Text = "查询通知";
+            // 
+            // 查询就业ToolStripMenuItem
+            // 
+            this.查询就业ToolStripMenuItem.Name = "查询就业ToolStripMenuItem";
+            this.查询就业ToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.查询就业ToolStripMenuItem.Text = "查询就业";
+            // 
+            // 查询饭卡ToolStripMenuItem
+            // 
+            this.查询饭卡ToolStripMenuItem.Name = "查询饭卡ToolStripMenuItem";
+            this.查询饭卡ToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.查询饭卡ToolStripMenuItem.Text = "查询饭卡";
+            // 
+            // 查询ToolStripMenuItem1
+            // 
+            this.查询ToolStripMenuItem1.Name = "查询ToolStripMenuItem1";
+            this.查询ToolStripMenuItem1.Size = new System.Drawing.Size(159, 26);
+            this.查询ToolStripMenuItem1.Text = "查询水电费";
+            this.查询ToolStripMenuItem1.Click += new System.EventHandler(this.QueryToolStripMenuItem1_Click);
+            // 
+            // 用户信息ToolStripMenuItem
+            // 
+            this.用户信息ToolStripMenuItem.Name = "用户信息ToolStripMenuItem";
+            this.用户信息ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.用户信息ToolStripMenuItem.Text = "用户信息";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // CourseInfo
+            // 
+            this.CourseInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CourseInfo.Location = new System.Drawing.Point(57, 81);
+            this.CourseInfo.Name = "CourseInfo";
+            this.CourseInfo.RowTemplate.Height = 27;
+            this.CourseInfo.Size = new System.Drawing.Size(973, 413);
+            this.CourseInfo.TabIndex = 1;
+            this.CourseInfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CourseInfo_CellContentClick);
+            // 
+            // TimingSelection
+            // 
+            this.TimingSelection.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TimingSelection.Location = new System.Drawing.Point(780, 555);
+            this.TimingSelection.Name = "TimingSelection";
+            this.TimingSelection.Size = new System.Drawing.Size(111, 38);
+            this.TimingSelection.TabIndex = 2;
+            this.TimingSelection.Text = "定时选课";
+            this.TimingSelection.UseVisualStyleBackColor = true;
+            this.TimingSelection.Click += new System.EventHandler(this.TimingSelection_Click);
+            // 
+            // Selection
+            // 
+            this.Selection.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Selection.Location = new System.Drawing.Point(942, 555);
+            this.Selection.Name = "Selection";
+            this.Selection.Size = new System.Drawing.Size(107, 38);
+            this.Selection.TabIndex = 3;
+            this.Selection.Text = "立即选课";
+            this.Selection.UseVisualStyleBackColor = true;
+            this.Selection.Click += new System.EventHandler(this.Selection_Click);
+            // 
+            // CourseSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 632);
+            this.Controls.Add(this.Selection);
+            this.Controls.Add(this.TimingSelection);
+            this.Controls.Add(this.CourseInfo);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CourseSelection";
+            this.Text = "course_selection";
+            this.Load += new System.EventHandler(this.Course_selection_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CourseInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 教务系统ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 选课ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查询培养方案ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 写请假条ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 进入学校论坛ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查询成绩ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查询通知ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查询就业ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查询饭卡ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查询ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 用户信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.DataGridView CourseInfo;
+        private System.Windows.Forms.Button TimingSelection;
+        private System.Windows.Forms.Button Selection;
+    }
+}
---
+++ team_development/CourseSelection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace team_development
+{
+    public partial class CourseSelection : Form
+    {
+        public CourseSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void Course_selection_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“visual_studioDataSet.course”中。您可以根据需要移动或删除它。
+
+
+        }
+
+        private void QueryToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AFLToolStripMenuItem_Click(object sender, EventArgs e)// ask for leave
+        {
+
+        }
+
+        private void TimingSelection_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void CourseInfo_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Selection_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
