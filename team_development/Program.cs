a51dabc91b4afcb599f7671ce1e8a9c47866131d
lingzerol <467264565@qq.com>
2019-04-21 10:05:38 +0800

    add log4net

--- team_development/Program.cs
+++ team_development/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using log4net;
 namespace team_development
 {
     static class Program
@@ -18,6 +18,7 @@ namespace team_development
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+           
         }
     }
 }
