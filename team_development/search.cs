8666d723a6d28a978371fa74616ca04730c1ae53
zengyujnu <45852849+zengyujnu@users.noreply.github.com>
2019-04-23 15:56:58 +0800

    zy

---
+++ team_development/search.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace team_development
+{
+    public partial class search : Form
+    {
+        public search()
+        {
+            InitializeComponent();
+        }
+
+        private void search_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
