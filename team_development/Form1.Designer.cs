6f478c91abe69776bdf777a0f6db66f889e60116
镇 张 <zhen183653821@outlook.com>
2019-04-24 18:05:53 +0800

    Form1

--- team_development/Form1.Designer.cs
+++ team_development/Form1.Designer.cs
@@ -29,6 +29,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.教务系统ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.选课ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -44,11 +45,15 @@
             this.用户信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.Form1Content = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
+            this.Form1Content.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.教务系统ToolStripMenuItem,
@@ -75,14 +80,14 @@
             // 选课ToolStripMenuItem
             // 
             this.选课ToolStripMenuItem.Name = "选课ToolStripMenuItem";
-            this.选课ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.选课ToolStripMenuItem.Size = new System.Drawing.Size(174, 26);
             this.选课ToolStripMenuItem.Text = "选课";
             this.选课ToolStripMenuItem.Click += new System.EventHandler(this.CourseSelectionToolStripMenuItem_Click);
             // 
             // MatchSchemeToolStripMenuItem
             // 
             this.MatchSchemeToolStripMenuItem.Name = "MatchSchemeToolStripMenuItem";
-            this.MatchSchemeToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.MatchSchemeToolStripMenuItem.Size = new System.Drawing.Size(174, 26);
             this.MatchSchemeToolStripMenuItem.Text = "查询培养方案";
             this.MatchSchemeToolStripMenuItem.Click += new System.EventHandler(this.MatchSchemeToolStripMenuItem_Click_1);
             // 
@@ -161,16 +166,38 @@
             // 
             // Form1Content
             // 
+            this.Form1Content.Controls.Add(this.panel1);
+            this.Form1Content.Controls.Add(this.label1);
             this.Form1Content.Location = new System.Drawing.Point(12, 55);
             this.Form1Content.Name = "Form1Content";
             this.Form1Content.Size = new System.Drawing.Size(1072, 565);
             this.Form1Content.TabIndex = 2;
             this.Form1Content.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1Content_Paint);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(219, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(625, 60);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "暨南大学学生自助软件";
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(246, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(552, 395);
+            this.panel1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(1096, 632);
             this.Controls.Add(this.Form1Content);
             this.Controls.Add(this.menuStrip1);
@@ -179,6 +206,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.Form1Content.ResumeLayout(false);
+            this.Form1Content.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,6 +230,8 @@
         private System.Windows.Forms.ToolStripMenuItem 用户信息ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
         private System.Windows.Forms.Panel Form1Content;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
