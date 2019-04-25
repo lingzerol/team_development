4dac4e9f97dc1918bcc369a2001381b7af5879ea
lingzerol <467264565@qq.com>
2019-04-25 10:23:08 +0800

    add Storage

---
+++ team_development/Lib/Storage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace team_development.Lib
+{
+    public static class Storage
+    {
+        private static BinaryFormatter bf;
+        static Storage() {
+            bf = new BinaryFormatter();
+        }
+        public static void Save(Object obj, string file)
+        {
+            try
+            {
+                Stream outStream = new FileStream(file, FileMode.OpenOrCreate);
+                bf.Serialize(outStream, obj);
+                outStream.Close();
+            }
+            catch (Exception e) {
+                Log.Error(e.ToString());
+            }
+        }
+        public static Object Load(string file) {
+            Object result = null;
+            try
+            {
+                Stream inStream = new FileStream(file, FileMode.Open);
+                result=bf.Deserialize(inStream);
+                inStream.Close();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.ToString());
+            }
+            return result;
+        }
+    }
+}
