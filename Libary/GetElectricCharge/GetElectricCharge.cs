2ca65c0151e37da045f8d6a230f12f45c77f5aef
lingzerol <467264565@qq.com>
2019-04-26 15:30:47 +0800

    add GetElectricCharge

---
+++ Libary/GetElectricCharge/GetElectricCharge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libary.GetElectricCharge
+{
+    public static class GetElectricCharge
+    {
+        static GetElectricCharge(){
+        }
+
+        public double GetCharge(string dormNum) {
+
+        }
+    }
+}
---
+++ team_developmentTests/GetElectricCharge/GetElectricChargeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Libary.GetElectricCharge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libary.GetElectricCharge.Tests
+{
+    [TestClass()]
+    public class GetElectricChargeTests
+    {
+        [TestMethod()]
+        public void GetChargeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
