5a0ba3c841f5c3c08d6011013dcefac55c570034
RedRin <446953045@qq.com>
2019-04-29 11:57:42 +0800

    爬取教务系统

--- Libary/GetElectricCharge/GetElectricCharge.cs
+++ Libary/GetElectricCharge/GetElectricCharge.cs
@@ -23,17 +23,17 @@ namespace Lib.GetElectricCharge
         {
             return 0;
         }
-        public static CookieContainer Login(string dormNum)
-        {
-            HttpHeader header=new HttpHeader();
-            header.accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
-            header.method = "POST";
-            header.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36";
-            header.maxTry = 5;
-            header.contentType = "application/x-www-form-urlencoded";
-            string data = "__LASTFOCUS=&__VIEWSTATE=%2FwEPDwULLTE5ODQ5MTY3NDlkZM4DISokA1JscbtlCdiUVQMwykIc&__VIEWSTATEGENERATOR=C2EE9ABB&__EVENTTARGET=&__EVENTARGUMENT=&__EVENTVALIDATION=%2FwEWBQLz%2BM6SBQK4tY3uAgLEhISACwKd%2B7q4BwKiwImNC7oxDnFDxrZR6l5WlUJDrpGZXrmN&hidtime=2019-04-26+19%3A53%3A31&txtname=" + dormNum + "&txtpwd=&ctl01=";
-            CookieContainer cookieContainer = HTMLHelper.GetCooKie("http://202.116.25.12/login.aspx",data , header);
-            return cookieContainer;
-        }
+        //public static CookieContainer Login(string dormNum)
+        //{
+        //    HttpHeader header = new HttpHeader();
+        //    header.accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+        //    header.method = "POST";
+        //    header.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36";
+        //    header.maxTry = 5;
+        //    header.contentType = "application/x-www-form-urlencoded";
+        //    string data = "__LASTFOCUS=&__VIEWSTATE=%2FwEPDwULLTE5ODQ5MTY3NDlkZM4DISokA1JscbtlCdiUVQMwykIc&__VIEWSTATEGENERATOR=C2EE9ABB&__EVENTTARGET=&__EVENTARGUMENT=&__EVENTVALIDATION=%2FwEWBQLz%2BM6SBQK4tY3uAgLEhISACwKd%2B7q4BwKiwImNC7oxDnFDxrZR6l5WlUJDrpGZXrmN&hidtime=2019-04-26+19%3A53%3A31&txtname=" + dormNum + "&txtpwd=&ctl01=";
+        //    CookieContainer cookieContainer = HTMLHelper.GetCooKie("http://202.116.25.12/login.aspx",data , header);
+        //    return cookieContainer;
+        //}
     }
 }
--- Libary/HTMLhelper.cs
+++ Libary/HTMLhelper.cs
@@ -14,26 +14,29 @@ namespace Lib
 {
     public class HTMLHelper
     {
+        public const int UTF8 = 1;
+        public const int DEFAULT = 2;
+
         CookieContainer cookies = null;
         string strCookies = string.Empty;
-        private static void setDefaultRequest(HttpHeader header, HttpWebRequest request)
-        {
-            request.Method = header.method;
-            request.Accept = header.accept;
-            request.UserAgent = header.userAgent;
-            request.ContentType = header.contentType;
-        }
-        //建立会话，并获取验证码
-        public Image GetValidateImage(string url,HttpHeader header)
+
+        /// <summary>
+        /// 并获取验证码
+        /// </summary>
+        /// <param name="url">验证码页面的url，如https://jwxt.jnu.edu.cn/ValidateCode.aspx</param>
+        /// <param name="header">请求头</param>
+        /// <returns></returns>
+        public Image GetValidateImage(string url, HttpHeader header)
         {
             cookies = new CookieContainer();
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Accept = header.accept;
             request.Method = header.method;
-            request.UserAgent =header.userAgent;
+            request.UserAgent = header.userAgent;
             request.CookieContainer = new CookieContainer(); //暂存到新实例
+            //MessageBox.Show("response之前：" + request.CookieContainer.GetCookieHeader(request.RequestUri));
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
+            //MessageBox.Show("response之后：" + request.CookieContainer.GetCookieHeader(request.RequestUri));
             //获取图片
             MemoryStream ms = null;
             using (var stream = response.GetResponseStream())
@@ -58,16 +61,108 @@ namespace Lib
             return img;
         }
 
-        public string Login(string url,HttpHeader header, CookieContainer cookieContainer, string postData)
+        /// <summary>
+        /// 登陆
+        /// </summary>
+        /// <param name="url">登陆页面，如https://jwxt.jnu.edu.cn/Login.aspx</param>
+        /// <param name="header">请求头</param>
+        /// <param name="postData">postdatas</param>
+        /// <returns></returns>
+        public string Login(string url, HttpHeader header, string postData)
+        {
+            Boolean setCookie = false;
+            HttpWebRequest request = null;
+            request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = header.method;
+            request.Accept = header.accept;
+            request.UserAgent = header.userAgent;
+            request.ContentType = header.contentType;
+            request.AllowAutoRedirect = true;
+            request.KeepAlive = true;
+
+            if (cookies == null)
+            {
+                setCookie = true;
+                cookies = new CookieContainer();
+                request.CookieContainer = new CookieContainer();
+            }
+            else
+            {
+                request.CookieContainer = cookies;
+            }
+
+            //MessageBox.Show("response之前：" + request.CookieContainer.GetCookieHeader(request.RequestUri));
+
+            if (header.method == "POST")
+            {
+                //将postdata放入请求
+                byte[] postdatabyte = Encoding.UTF8.GetBytes(postData);
+                request.ContentLength = postdatabyte.Length;
+                using (Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(postdatabyte, 0, postdatabyte.Length);
+                }
+            }
+
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse(); //账号，密码，验证码输错了会出错
+            //MessageBox.Show("response之后：" + request.CookieContainer.GetCookieHeader(request.RequestUri));
+            //MessageBox.Show(response.Headers.Get("Set-Cookie"));
+
+            if (setCookie)
+            {
+                cookies = request.CookieContainer; //将cookies保存到本对象中
+                strCookies = request.CookieContainer.GetCookieHeader(request.RequestUri); //字符串格式的cookie
+            }
+
+            string strWebData = string.Empty;
+
+            if (header.encoding == HTMLHelper.UTF8)
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    strWebData = reader.ReadToEnd();
+                }
+            }
+            else
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.Default))
+                {
+                    strWebData = reader.ReadToEnd();
+                }
+            }
+
+            response.Close();
+            return strWebData;
+        }
+
+
+        /// <summary>
+        /// POST请求
+        /// </summary>
+        /// <param name="url">请求的页面</param>
+        /// <param name="header">请求头</param>
+        /// <param name="postData">data</param>
+        /// <returns></returns>
+        public string Post(string url, HttpHeader header, string postData)
         {
             HttpWebRequest request = null;
             request = (HttpWebRequest)WebRequest.Create(url);
-            setDefaultRequest(header, request);
+            request.Method = "POST";//header.method;
+            request.Accept = header.accept;
+            request.UserAgent = header.userAgent;
+            request.ContentType = header.contentType;
             request.AllowAutoRedirect = true;
-            request.CookieContainer = cookieContainer;
             request.KeepAlive = true;
 
-            //string postData = string.Format("username={0}&password={1}&Validate={2}&isautologin=1&Submit=", txtUserName.Text, txtPassword.Text, txtValidate.Text);  //这里按照前面FireBug中查到的POST字符串做相应修改。
+            if (cookies == null)
+            {
+                return "";
+            }
+            else
+            {
+                request.CookieContainer = cookies;
+            }
 
             //将postdata放入请求
             byte[] postdatabyte = Encoding.UTF8.GetBytes(postData);
@@ -77,54 +172,91 @@ namespace Lib
                 stream.Write(postdatabyte, 0, postdatabyte.Length);
             }
 
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse(); //账号，密码，验证码输错了会出错
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
             string strWebData = string.Empty;
-            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.Default))
+
+            if (header.encoding == HTMLHelper.UTF8)
             {
-                strWebData = reader.ReadToEnd();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    strWebData = reader.ReadToEnd();
+                }
             }
+            else
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.Default))
+                {
+                    strWebData = reader.ReadToEnd();
+                }
+            }
+
             response.Close();
             return strWebData;
         }
 
-        public string Post(string url, string postData) {
 
-        }
-
-        public static string GetHtml(string getUrl, CookieContainer cookieContainer, HttpHeader header)
+        /// <summary>
+        /// GET请求
+        /// </summary>
+        /// <param name="url">请求的页面</param>
+        /// <param name="header">请求头</param>
+        /// <returns></returns>
+        public string Get(string url, HttpHeader header)
         {
             HttpWebRequest request = null;
-            HttpWebResponse httpWebResponse = null;
-            try
-            {
-                request = (HttpWebRequest)HttpWebRequest.Create(getUrl);
-                setDefaultRequest(header,request);
-                request.CookieContainer = cookieContainer;
-                request.ServicePoint.ConnectionLimit = header.maxTry;
-                request.Referer = getUrl;
+            request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";//header.method;
+            request.Accept = header.accept;
+            request.UserAgent = header.userAgent;
+            request.ContentType = header.contentType;
+            request.AllowAutoRedirect = true;
+            request.KeepAlive = true;
 
-                httpWebResponse = (HttpWebResponse)request.GetResponse();
-                Stream responseStream = httpWebResponse.GetResponseStream();
-                StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
-                string html = streamReader.ReadToEnd();
-                streamReader.Close();
-                responseStream.Close();
-                request.Abort();
-                httpWebResponse.Close();
-                return html;
-            }
-            catch (Exception e)
+            if (cookies == null)
             {
-                if (request != null) request.Abort();
-                if (httpWebResponse != null) httpWebResponse.Close();
-                return string.Empty;
+                return "";
             }
+            else
+            {
+                request.CookieContainer = cookies;
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            string strWebData = string.Empty;
+
+            if (header.encoding == HTMLHelper.UTF8)
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    strWebData = reader.ReadToEnd();
+                }
+            }
+            else
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.Default))
+                {
+                    strWebData = reader.ReadToEnd();
+                }
+            }
+
+            response.Close();
+            return strWebData;
         }
     }
 
     public class HttpHeader
     {
+        public HttpHeader()
+        {
+            method = "POST";
+            accept = "*/*;";
+            userAgent = "Mozilla/5.0";
+            contentType = "application/x-www-form-urlencoded";
+            encoding = HTMLHelper.DEFAULT;
+        }
+
         public string contentType { get; set; }
 
         public string accept { get; set; }
@@ -133,7 +265,6 @@ namespace Lib
 
         public string method { get; set; }
 
-        public int maxTry { get; set; }
-
+        public int encoding { get; set; }
     }
 }
