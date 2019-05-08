7c8f03fd480855875a2f6143f37c9fe6197d3bda
zengyujnu <45852849+zengyujnu@users.noreply.github.com>
2019-05-09 00:19:10 +0800

    Changed the name of water charge for electricity charge, changed the interface and code for inquiring electricity charge, and increased the log and test of electricity charge

--- team_development/UI/Query/QueryElectricity/QueryElectricity.cs
+++ team_development/UI/Query/QueryElectricity/QueryElectricity.cs
@@ -11,34 +11,46 @@ using System.Windows.Forms;
 using System.Threading;
 using Lib.GetElectricCharge;
 
-namespace team_development.UI.QueryWaterElectricity
+namespace team_development.UI.QueryElectricity
 {
-    public partial class QueryWaterElectricity : Form
+    public partial class QueryElectricity : Form
     {
         AutoResetEvent obj = new AutoResetEvent(false);//使用信号量
         private GetElectricCharge gec = new GetElectricCharge();
         private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
         //使页面加载完毕
 
-        public QueryWaterElectricity()
+        public QueryElectricity()
         {
             InitializeComponent();
-            timer.Interval = 5000;
+            timer.Interval = 2000;
             timer.Tick += new EventHandler(timerTick);
             timer.Stop();
         }
 
-        private void btn_Login_Click(object sender, EventArgs e)
+        private void btn_Search_Click(object sender, EventArgs e)
         {
             gec.Login(Dorm.Text);
             timer.Start();
-
         }
 
         private void timerTick(object sender, EventArgs e)
         {
-            timer.Stop();
-            result.Text=gec.GetElectric();
+            try
+            {
+                int num;
+                string elec = gec.GetElectric();
+                int.TryParse(elec, out num);
+                while (num >= 1000)
+                {
+                    timer.Stop();
+                }
+                result.Text = gec.GetElectric();
+            }
+            catch (Exception E)
+            {
+                Log.log.Error(E.ToString());
+            }
         }
 
         private void TurnToForm(Form f)
@@ -47,12 +59,22 @@ namespace team_development.UI.QueryWaterElectricity
             ((Form1)(this.ParentForm)).TurnForm(f);
         }
 
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result.Text = gec.GetElectric();
+            }
+            catch (Exception E)
+            {
+                Log.log.Error(E.ToString());
+            }
+        }
 
 
-        private void QueryWaterElectricity_Load(object sender, EventArgs e)
+        private void QueryElectricity_Load(object sender, EventArgs e)
         {
 
         }
-
     }
 }
