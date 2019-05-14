3e5fddda235f9b202653bfe0e29bc488e437b1a0
zengyujnu <45852849+zengyujnu@users.noreply.github.com>
2019-05-15 01:11:30 +0800

    add the search about news and notice.

---
+++ team_developmentTests/SearchTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lib.Tests
+{
+    [TestClass]
+    public class SearchTest
+    {
+        NoticeSearch search = new NoticeSearch();
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            search.getHtml("6");
+        }
+
+        [TestMethod]
+        public void getHtmlTest()
+        {
+            search.getHtml("6");
+        }
+
+        [TestMethod]
+        public void getTextTest()
+        {
+            if (search.getText().Equals(null))
+            {
+                Assert.Fail();
+            }
+            
+        }
+    }
+}
