21070f14d416ef0378a6ba271ea5913e4fa11118
zengyujnu <45852849+zengyujnu@users.noreply.github.com>
2019-06-06 01:08:04 +0800

    add QueryElectricTest

---
+++ team_developmentTests/QueryElectricTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lib.GetElectricCharge;
+using System.Threading;
+
+namespace team_developmentTests
+{
+    [TestClass]
+    public class QueryElectricTest
+    {
+        GetElectricCharge gec = new GetElectricCharge();
+        Mutex charge_mtx = new Mutex();
+        string electric;
+
+        public void SetElectricCharge(string charge)
+        {
+            charge_mtx.WaitOne();
+            charge_mtx.ReleaseMutex();
+            electric = charge;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            gec.GetElectric("3305", new SetCharge(this.SetElectricCharge));
+            if (electric == null)
+            {
+                //Assert.Fail();
+            }
+        }
+    }
+}
